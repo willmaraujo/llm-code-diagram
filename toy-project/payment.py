787814273a2f400e2943748925c004e906b24ac7
Williams Araujo <williamsa@ats4it.com>
2025-04-26 14:24:53 +0200

    Removing everything to start from scratch with javascript project to analyse

--- toy-project/payment.py
+++
@@ -1,9 +0,0 @@
-# payment.py
-
-class PaymentProcessor:
-    def process_payment(self, amount):
-        """Process payment if the amount is valid."""
-        if amount <= 0:
-            raise ValueError("Invalid amount")
-        print(f"Processed payment of ${amount}")
-        return True
